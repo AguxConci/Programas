046678709324c6d0311c1f2a0bf9d2b76ffb9cdd
Agustin Conci <aguxconci@gmail.com>
2023-09-14 19:52:12 -0300

    Operadores JavaScript

---
+++ Index.js
@@ -0,0 +1,21 @@
+console.log("Hola desde el archivo de JS")
+let Algo = "banana"
+const pi = 3.1415926535
+let nombre 
+let numero = 10
+/* if (confirm("Te gusta mi pagina") && nombre == "agus" || nombre == "Agus") {
+    alert("Hola master")
+} else if ("Irina") {
+    alert("Hola Reina")
+} else {
+    alert("hola pete")
+} */
+
+do{
+    nombre = prompt("Como te llamas")
+    if (nombre == "Agus" || nombre == "agus"){
+        mostrar = true 
+    } else {
+        mostrar = false
+    } 
+} while (nombre == null || nombre == undefined || nombre == "")
