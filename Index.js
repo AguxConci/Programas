bdddf594928127f7dfc2e1a3ca47a9b581538ca1
Agustin Conci <aguxconci@gmail.com>
2023-09-20 01:41:25 -0300

    Tipos de datos Too

--- Index.js
+++ Index.js
@@ -1,38 +1,46 @@
 console.log("Hola desde el archivo de JS")
 let Algo = "banana"
-const pi = 3.1415926535
+ const pi = 3.14159000900
 let nombre 
 let numero = 10
 
+// function sumar() {
+// 	let resultado = 12 + 23;
+// 	 return resultado ;
+ 
+//  }
+
 /* 
 if (confirm("Te gusta mi pagina") && nombre == "agus" || nombre == "Agus") {
-    alert("Hola master")
+	alert("Hola master")
 } else if ("Irina") {
-    alert("Hola Reina")
+	alert("Hola Reina")
 } else {
-    alert("hola pete")
+	alert("hola pete")
 } */
 
 /*
 do{
-    nombre = prompt("Como te llamas")
-    if (nombre == "Agus" || nombre == "agus"){
-        mostrar = true 
-    } else {
-        mostrar = false
-    } 
+	nombre = prompt("Como te llamas")
+	if (nombre == "Agus" || nombre == "agus"){
+		mostrar = true 
+	} else {
+		mostrar = false
+	} 
 } while (nombre == null || nombre == undefined || nombre == "")
 
 function prueba() {
-    alert("Prueba")
+	alert("Prueba")
 }
 */
 
-const verificar = () => {
-    nombre = prompt("Decime tu nombre")
-    if (nombre == "Agus" || nombre == "agus" || nombre == "") {
-    return true 
-    } else {
-       return false
-    }
-}
+	const verificar = () => {
+		nombre = prompt("Decime tu nombre")
+		if (nombre == "Agus" || nombre == "agus" || nombre == "") {
+			return true 
+		} else {
+		return	false
+	}
+	}
+	
+	
---
+++ Pruebas.js
@@ -0,0 +1,10 @@
+const p = 3.1
+let a = prompt()
+
+function sumar(a,b) {
+   let resultado = a + b;
+    return resultado ;
+
+}
+
+let suma = sumar()
